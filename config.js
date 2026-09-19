3a5c1596da8d29ea9f25beea38b20519c7ef5ff3
chain357 <chain6868@gmail.com>
2026-09-19 10:53:05 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,3 @@
+// ตั้งชื่อตัวแปรตามข้อกำหนด ห้ามใช้ชื่อ supabase ซ้ำกับ core library
+const SUPABASE_URL = "https://yllvjlijzymmwqlyvlcg.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_cbcvj1nvoYJZLmM4w20Hjg_wDrwjMxY";
